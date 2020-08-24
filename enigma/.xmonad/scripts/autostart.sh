7cd3d7e3515ab857a3f7b9ede05df2be20b9487d
Sagnik Bhattacharya <sagnikdps@gmail.com>
2020-08-23 21:57:28 -0700

    first draft

--- enigma/.xmonad/scripts/autostart.sh
+++ enigma/.xmonad/scripts/autostart.sh
@@ -26,27 +26,27 @@ function run {
 xsetroot -cursor_name left_ptr &
 
 #start ArcoLinux Welcome App
-run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+# run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 
 #Some ways to set your wallpaper besides variety or nitrogen
-sh .fehbg
+nitrogen ~/.config/wallpapers
 #start the conky to learn the shortcuts
 (conky -c $HOME/.xmonad/scripts/system-overview) &
 
 #starting utility applications at boot time
 #run variety &
-run nm-applet &
+# run nm-applet &
 #run pamac-tray &
 #run xfce4-power-manager &
 #run volumeicon &
 exec ksuperkey -e 'Super_L=Super_L|d' &
 numlockx on &
 #blueberry-tray &
-urxvtd &
+# urxvtd &
 picom --config $HOME/.xmonad/scripts/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
-exec mpd
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# /usr/lib/xfce4/notifyd/xfce4-notifyd &
+# exec mpd
 
 #starting user applications at boot time
 #nitrogen --restore &
